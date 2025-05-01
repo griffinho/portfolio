3607d7f6c31df25b77ddaa152d1c2022b719d517
griffinho <erikcaue2002@gmail.com>
2025-05-01 15:39:04 -0300

    Projeto inicial

--- my-portfolio/src/app/app.config.ts
+++ my-portfolio/src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { NbThemeModule } from '@nebular/theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()]
